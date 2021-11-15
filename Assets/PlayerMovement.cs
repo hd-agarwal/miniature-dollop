920c3d8199e175fd3c2db074dd107245dd990b7e
hd-agarwal <harshdhar2000@gmail.com>
2021-11-15 19:02:05 +0530

    force added on player block

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody playerRB;
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerRB.AddForce(0,0,2000*Time.deltaTime);
     }
 }
